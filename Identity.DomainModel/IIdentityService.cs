c2471a29e263df853b73df876b3b976618705a29
Andreas Fridlund <afrxx09@student.lnu.se>
2014-12-18 06:51:29 +0100

    Fungerande test, del 1

---
+++ Identity.DomainModel/IIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.DomainModel
+{
+    public interface IIdentityService : IDisposable
+    {
+        //metoder mot service lagret
+        //Exemeplvis GetLoactions i projekt-applikationen
+    }
+}
